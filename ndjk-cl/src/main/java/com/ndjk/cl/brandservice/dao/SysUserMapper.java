42e205394045930eb5f6906022fd656bfdedd0c4
wanglei <lei.wang@fuyinhy.com>
2018-01-31 09:09:28 +0800

    update user

--- ndjk-cl/src/main/java/com/ndjk/cl/brandservice/dao/SysUserMapper.java
+++ ndjk-cl/src/main/java/com/ndjk/cl/brandservice/dao/SysUserMapper.java
@@ -3,6 +3,7 @@ package com.ndjk.cl.brandservice.dao;
 import com.ndjk.cl.brandinteraction.model.ContentManage;
 import com.ndjk.cl.brandinteraction.model.vo.ContentManageVo;
 import com.ndjk.cl.brandservice.model.SysUser;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -22,6 +23,6 @@ public interface SysUserMapper {
     SysUser selectByLoginName(String loginName);
 
     //用户列表
-    List<SysUser> listAll(SysUser sysUser, int currentPage, int pageSize);
+    List<SysUser> listAll(@Param("sysUser") SysUser sysUser,@Param("offset") int offset, @Param("pageSize") int pageSiz);
 
 }
